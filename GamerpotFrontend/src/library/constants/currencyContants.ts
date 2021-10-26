c3f5ac3d24dadc7b092ff3b63094d61b7c84f30e
Eusebio Ajas Santos <eusebioajas6@gmail.com>
2021-10-26 12:32:05 -0500

    Add currency constants

---
+++ GamerpotFrontend/src/library/constants/currencyContants.ts
@@ -0,0 +1,3 @@
+export const US_CURRENCY_CODE = 'USD';
+export const MX_CURRENCY_CODE = 'MXN';
+export const DEFAULT_CURRENCY_AMOUNT = '10';
--- GamerpotFrontend/src/library/constants/dealsConstants.ts
+++ GamerpotFrontend/src/library/constants/dealsConstants.ts
@@ -10,3 +10,4 @@ export const DEFAULT_URL_PARAMS: DealsSearchParams = {
 
 export const API_DEALS_URL = 'https://www.cheapshark.com/api/1.0/deals?';
 export const API_STORES_URL = 'https://www.cheapshark.com/api/1.0/stores';
+export const API_CURRENCY_URL = 'https://free.currconv.com/api/v7/convert';
