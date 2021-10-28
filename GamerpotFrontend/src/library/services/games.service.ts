89de91dd07a67629ba2cdc1410d83ac1771344b7
Shadic78 <carlosjosue7889@gmail.com>
2021-10-28 10:59:56 -0500

    Added a service to get a game specific data

--- GamerpotFrontend/src/library/services/games.service.ts
+++ GamerpotFrontend/src/library/services/games.service.ts
@@ -1,11 +1,23 @@
 import {buildParams} from './paramBuilder';
-import {DEFAULT_URL_PARAMS, API_GAMES_URL} from '../constants/gamesConstants';
+import {
+  DEFAULT_URL_PARAMS,
+  API_GAMES_URL,
+  getGameInfoUrl,
+} from '../constants/gamesConstants';
 import {GamesSearchParams} from '../models/gamesSearchParams';
 import {GameResponse} from '../models/gameResponse';
+import {GameDetails} from '../models/gameDetails';
 
 export const getGames = async (params?: GamesSearchParams) => {
   const urlParams = params || DEFAULT_URL_PARAMS;
   const response = await fetch(`${API_GAMES_URL}${buildParams(urlParams)}`);
   const games: GameResponse = await response.json();
   return games;
+};
+
+export const getGameInfo = async (gameId: number) => {
+  const url = getGameInfoUrl(gameId);
+  const response = await fetch(url);
+  const gameInfo: GameDetails = await response.json();
+  return gameInfo;
 };
