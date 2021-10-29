6707b2b873a9bcff835cb562068ef1f27cb59cd6
Shadic78 <carlosjosue7889@gmail.com>
2021-10-29 11:36:47 -0500

    Deleted lines on HomeScreen

--- GamerpotFrontend/src/library/constants/gamesConstants.ts
+++ GamerpotFrontend/src/library/constants/gamesConstants.ts
@@ -1,4 +1,5 @@
 import {GamesSearchParams} from '../models/gamesSearchParams';
+import {RAWG_API_KEY} from '@env';
 
 export const DEFAULT_URL_PARAMS: GamesSearchParams = {
   page: 1,
@@ -15,7 +16,7 @@ export const GAMES_ORDERING: string[] = [
   'metacritic',
 ];
 
-export const API_KEY = 'f3f82f92ef90498e986b88325fcf5e62';
+export const API_KEY = RAWG_API_KEY;
 export const API_GAMES_URL = `https://api.rawg.io/api/games?key=${API_KEY}&`;
 export const API_PLATFORMS_URL = `https://api.rawg.io/api/platforms?key=${API_KEY}`;
 
