35249d0ce2dbac2126c891bf383591ab9857be55
Shadic78 <carlosjosue7889@gmail.com>
2021-10-29 11:36:47 -0500

    Deleted lines on HomeScreen

--- GamerpotFrontend/src/screens/HomeScreen/index.tsx
+++ GamerpotFrontend/src/screens/HomeScreen/index.tsx
@@ -1,48 +1,20 @@
-import React, {useEffect, useState} from 'react';
-import {Button, Text, View, ScrollView} from 'react-native';
+import React from 'react';
+import {Button, Text, View} from 'react-native';
 import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 import {useNavigation} from '@react-navigation/core';
 import {RootStackParamList} from './RootStackParams';
 import {flex} from '../../components/styles';
-import {getGames} from '../../library/services/games.service';
-import {GameDetails} from '../../library/models/gameDetails';
-import {GameResponse} from '../../library/models/gameResponse';
-import {GamesSearchParams} from '../../library/models/gamesSearchParams';
 
 type homeScreenParams = NativeStackNavigationProp<RootStackParamList, 'Home'>;
-const params: GamesSearchParams = {
-  page: 1,
-  page_size: 5,
-};
 
 const HomeScreen = () => {
   const navigation = useNavigation<homeScreenParams>();
-  const [games, setGames] = useState([] as GameDetails[]);
-
-  useEffect(() => {
-    const fetchGamesData = async () => {
-      const response: GameResponse = await getGames(params);
-      setGames(response.results);
-    };
-    fetchGamesData();
-  }, []);
 
   return (
     <View style={flex.container}>
       <Text>Home screen</Text>
       <Button title="Go Login" onPress={() => navigation.navigate('Login')} />
       <Button title="Go Deals" onPress={() => navigation.navigate('Deals')} />
-      <ScrollView>
-        {games &&
-          games.map((game, index) => {
-            return (
-              <View key={index}>
-                <Text>{game.name}</Text>
-                <Text>{game.id}</Text>
-              </View>
-            );
-          })}
-      </ScrollView>
     </View>
   );
 };
--- GamerpotFrontend/src/type/env.d.ts
+++ GamerpotFrontend/src/type/env.d.ts
@@ -1,3 +1,4 @@
 declare module '@env' {
   export const CURRENCY_API_KEY: string;
+  export const RAWG_API_KEY: string;
 }
