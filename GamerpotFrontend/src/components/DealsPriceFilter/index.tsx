414d4a8569e5c25f6339386ff8a1c31e24985901
Shadic78 <carlosjosue7889@gmail.com>
2021-11-02 10:14:50 -0600

    Refactored DealsPriceFilter

--- GamerpotFrontend/src/components/DealsPriceFilter/index.tsx
+++ GamerpotFrontend/src/components/DealsPriceFilter/index.tsx
@@ -2,34 +2,36 @@ import React, {useState} from 'react';
 import {View, Text, TextInput} from 'react-native';
 import {styles} from './styles';
 import {DealsPriceFilterProps} from './props';
+import TextBoxWithLabel from '../TextBoxWithLabel';
 
 const DealsPriceFilter = ({onPriceChange}: DealsPriceFilterProps) => {
   const [minPrice, setMinPrice] = useState('');
   const [maxPrice, setMaxPrice] = useState('');
+  const inputs = [
+    {
+      value: minPrice,
+      name: 'lowerPrice',
+      label: 'Minimum',
+      onChange: setMinPrice,
+      onSubmit: onPriceChange,
+    },
+    {
+      value: maxPrice,
+      name: 'upperPrice',
+      label: 'Maximum',
+      onChange: setMaxPrice,
+      onSubmit: onPriceChange,
+    },
+  ];
 
   return (
     <View style={styles.container}>
       <View style={styles.row}>
         <Text style={styles.title}>Filter by price</Text>
       </View>
-      <View style={styles.row}>
-        <Text style={styles.labels}>Minimum: </Text>
-        <TextInput
-          value={minPrice}
-          style={styles.inputs}
-          onChangeText={setMinPrice}
-          onSubmitEditing={() => onPriceChange('lowerPrice', minPrice)}
-        />
-      </View>
-      <View style={styles.row}>
-        <Text style={styles.labels}>Maximum: </Text>
-        <TextInput
-          value={maxPrice}
-          style={styles.inputs}
-          onChangeText={setMaxPrice}
-          onSubmitEditing={() => onPriceChange('upperPrice', maxPrice)}
-        />
-      </View>
+      {inputs.map((input, index) => (
+        <TextBoxWithLabel key={index} {...input} />
+      ))}
     </View>
   );
 };
--- GamerpotFrontend/src/components/TextBoxWithLabel/styles.ts
+++ GamerpotFrontend/src/components/TextBoxWithLabel/styles.ts
@@ -3,19 +3,25 @@ import {colors} from '../../library/constants/colors';
 import {fonts} from '../../library/constants/fonts';
 
 export const styles = StyleSheet.create({
-  container: {
-    marginBottom: 20,
-  },
   row: {
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-between',
     marginBottom: 8,
   },
-  title: {
-    fontFamily: fonts.primary,
-    fontSize: 18,
+  labels: {
     color: colors.secondary,
-    marginBottom: 10,
+    marginRight: 8,
+    fontSize: 16,
+    fontFamily: fonts.secondary,
+  },
+  inputs: {
+    backgroundColor: colors.lightGray,
+    color: colors.secondary,
+    textAlign: 'left',
+    width: 250,
+    padding: 5,
+    paddingLeft: 15,
+    fontSize: 14,
   },
 });
