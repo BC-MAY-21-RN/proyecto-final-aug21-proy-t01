cdb9ded7b66c89d5ab40f41033a2b3b69ed0b8ba
Eusebio Ajas Santos <eusebioajas6@gmail.com>
2021-11-03 11:58:52 -0600

    Add config for news service - Add: - api key - api base url

---
+++ GamerpotFrontend/src/library/constants/newsConstants.ts
@@ -0,0 +1,3 @@
+import {GAMESPOT_API_KEY} from '@env';
+
+export const API_GAMESPOT_URL = `http://www.gamespot.com/api/articles/?api_key=${GAMESPOT_API_KEY}&format=json`;
--- GamerpotFrontend/src/type/env.d.ts
+++ GamerpotFrontend/src/type/env.d.ts
@@ -1,4 +1,5 @@
 declare module '@env' {
   export const CURRENCY_API_KEY: string;
   export const RAWG_API_KEY: string;
+  export const GAMESPOT_API_KEY: string;
 }
