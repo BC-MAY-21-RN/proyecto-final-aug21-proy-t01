6d37263a75632554b126b1adeed728e94973e0bf
Eusebio Ajas Santos <eusebioajas6@gmail.com>
2021-11-05 09:32:17 -0600

    Add default url params
    - I've added model for search params
    - I've added default params for gamespot api queries

--- GamerpotFrontend/src/library/services/news.service.ts
+++ GamerpotFrontend/src/library/services/news.service.ts
@@ -1,8 +1,14 @@
-import {API_GAMESPOT_URL} from '../constants/newsConstants';
 import {NewsResponse} from '../models/newsResponse';
+import {NewsSearchParams} from '../models/newsSearchParams';
+import {buildParams} from './paramBuilder';
+import {
+  API_GAMESPOT_URL,
+  DEFAULT_GAMESPOT_URL_PARAMS,
+} from '../constants/newsConstants';
 
-export const getNews = async () => {
-  const response = await fetch(API_GAMESPOT_URL);
+export const getNews = async (params?: NewsSearchParams) => {
+  const urlParams = params || DEFAULT_GAMESPOT_URL_PARAMS;
+  const response = await fetch(`${API_GAMESPOT_URL}${buildParams(urlParams)}`);
   const newsResponse: NewsResponse = await response.json();
   return newsResponse.results;
 };
