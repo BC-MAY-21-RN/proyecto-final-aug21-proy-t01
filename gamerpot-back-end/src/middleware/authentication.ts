ff02a13ff08a6ce146e005ebd69cefb411441b07
Eusebio Ajas Santos <eusebioajas6@gmail.com>
2021-11-23 10:20:44 -0600

    Add authentication middleware

---
+++ gamerpot-back-end/src/middleware/authentication.ts
@@ -0,0 +1,19 @@
+import { Request, Response, NextFunction} from 'express'
+import { JWT } from "../config/jwt";
+import jwt from "jsonwebtoken";
+
+export const authenticate = (req: Request, res: Response, next: NextFunction) => {
+  if (!req.headers['authorization']){
+    res.status(401).json({message: 'You are not authenticated'})
+  }
+
+  const token = req.headers['authorization']!.split(' ')[1]
+  jwt.verify(token, JWT.SECRET, (error, decoded) => {
+    if(error){
+      res.status(401).json({message: 'Token can not be decoded'})
+    }else{
+      req.user = decoded;
+      next();
+    }
+  })
+}
