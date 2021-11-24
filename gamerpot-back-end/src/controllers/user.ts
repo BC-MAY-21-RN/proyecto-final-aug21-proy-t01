3d61b9c9f09f3d82ed93b085c88813ff7e49e7fa
Eusebio Ajas Santos <eusebioajas6@gmail.com>
2021-11-24 13:16:16 -0600

    Add endpoints for user login and register
    - I've added controlers, routes for user login, register and update user

---
+++ gamerpot-back-end/src/controllers/user.ts
@@ -0,0 +1,112 @@
+import { Request, Response } from "express";
+import { getCustomRepository } from "typeorm";
+import { UserRepository } from "../repositories/user.repository";
+import { User } from "../entities/user.entity";
+import bcrypt from "bcrypt";
+import { JWT } from "../config/jwt";
+import jwt from "jsonwebtoken";
+
+export const getUsers = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  const userRepository = getCustomRepository(UserRepository);
+  try {
+    const users = await userRepository.find();
+    return res.send(users).status(200);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+};
+
+export const getUser = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  const userRepository = getCustomRepository(UserRepository);
+  try {
+    const foundUser = await userRepository.findOne(req.params.userId);
+    return res.status(200).json(foundUser);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+};
+
+export const signIn = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  const { email, password } = req.body;
+  const userRepository = getCustomRepository(UserRepository);
+  try {
+    const foundUser = await userRepository.findOne({ where: { email } });
+    if (!foundUser) {
+      res.status(404).json({ message: "Email does not exist" });
+    }
+
+    const isPasswordMatch = await bcrypt.compare(password, foundUser!.password);
+
+    if (isPasswordMatch) {
+      const token = jwt.sign({ foundUser }, JWT.JWT_SECRET, {
+        expiresIn: JWT.EXPIRATION_TIME,
+      });
+      return res.status(200).json(token);
+    }
+    return res.status(400).json({ message: "Credentials does not match" });
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+};
+
+export const signUp = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  const { name, username, email, password, description, profilePictureUrl } =
+    req.body;
+  const userRepository = getCustomRepository(UserRepository);
+
+  try {
+    const foundUser = await userRepository.findOne({ where: { email } });
+    if (foundUser) res.status(404).json({ message: "Email already exists" });
+
+    const encryptedPassword = await bcrypt.hash(password, 10);
+    const newUser = await userRepository.save(
+      new User({
+        name,
+        username,
+        email,
+        password: encryptedPassword,
+        description,
+        profilePictureUrl,
+      })
+    );
+    return res.status(200).json(newUser);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+};
+
+export const updateUser = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  const userRepository = getCustomRepository(UserRepository);
+  try {
+    const foundUser = await userRepository.findOne(req.params.userId);
+
+    if (!foundUser) {
+      res.status(404).json({ message: "User not found" });
+    }
+
+    const updatedUser = await userRepository.save({
+      ...foundUser,
+      description: req.body.description,
+      profilePictureUrl: req.body.profilePictureUrl,
+    });
+
+    return res.status(200).json(updatedUser);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+};
